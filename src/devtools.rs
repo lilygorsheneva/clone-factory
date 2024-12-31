d498b94cb9314846882ef5697ad9c307b4388993
Lily <lilygorsheneva@gmail.com>
2024-12-30 18:26:29 -0800

    Use relative direction in sample recording.

--- src/devtools.rs
+++ src/devtools.rs
@@ -1,25 +1,29 @@
 use crate::action::{Action, SubAction};
 use crate::datatypes::{ActionQueue, Recording};
-use crate::direction::AbsoluteDirection;
+use crate::direction::RelativeDirection;
 
 pub fn make_sample_recording() -> Recording {
     Recording {
         command_list: ActionQueue {
             q: vec![
                 Action {
-                    direction: crate::direction::Direction::Absolute(AbsoluteDirection::N),
+                    direction: crate::direction::Direction::Relative(RelativeDirection::F),
                     action: SubAction::Move,
                 },
                 Action {
-                    direction: crate::direction::Direction::Absolute(AbsoluteDirection::W),
+                    direction: crate::direction::Direction::Relative(RelativeDirection::R),
                     action: SubAction::Move,
                 },
                 Action {
-                    direction: crate::direction::Direction::Absolute(AbsoluteDirection::S),
+                    direction: crate::direction::Direction::Relative(RelativeDirection::R),
                     action: SubAction::Move,
                 },
                 Action {
-                    direction: crate::direction::Direction::Absolute(AbsoluteDirection::E),
+                    direction: crate::direction::Direction::Relative(RelativeDirection::R),
+                    action: SubAction::Move,
+                },
+                Action {
+                    direction: crate::direction::Direction::Relative(RelativeDirection::R),
                     action: SubAction::Move,
                 },
             ],
