83c9fa69b03dc0871e21034026d841d183f1b5d9
Lily <lilygorsheneva@gmail.com>
2025-01-24 12:18:50 -0800

    Allow actors to despawn due to paradox.

--- src/actor.rs
+++ src/actor.rs
@@ -1,19 +1,24 @@
 //! A player or npc.
-use crate::engine::tracking_worldlayer::{Trackable, TrackableId};
-use crate::recording::{Recording, db::RecordingId};
-use crate::datatypes::Coordinate;
-use crate::game_state::db::ActorId;
-use crate::direction::AbsoluteDirection;
-use crate::inventory::BasicInventory;
+use std::cmp::min;
 
-#[derive(PartialEq, Debug)]
-#[derive(Copy, Clone)]
+use crate::action::Action;
+use crate::datatypes::Coordinate;
+use crate::direction::AbsoluteDirection;
+use crate::engine::tracking_worldlayer::{Trackable, TrackableId};
+use crate::engine::update::{self, Delta, UpdatableContainer, UpdatableContainerDelta};
+use crate::error::{Result, Status::Error};
+use crate::game_state::db::ActorId;
+use crate::game_state::game::{Game, GameUpdate};
+use crate::inventory::BasicInventory;
+use crate::recording::{db::RecordingId, Recording};
+
+#[derive(PartialEq, Debug, Copy, Clone)]
 pub struct Actor {
     pub facing: AbsoluteDirection,
     pub isplayer: bool,
     pub actor_id: ActorId,
     pub inventory: BasicInventory,
-    pub paradox_level: f64
+    pub paradox_level: f64,
 }
 
 impl Actor {
@@ -23,7 +28,7 @@ impl Actor {
             isplayer: false,
             actor_id: ActorId::DEFAULT,
             inventory: Default::default(),
-            paradox_level: 0.0
+            paradox_level: 0.0,
         }
     }
 
@@ -32,8 +37,8 @@ impl Actor {
             facing: AbsoluteDirection::N,
             isplayer: false,
             actor_id: ActorId::DEFAULT,
-            inventory:recording.inventory,
-            paradox_level: 0.0
+            inventory: recording.inventory,
+            paradox_level: 0.0,
         }
     }
 
@@ -48,4 +53,35 @@ impl Trackable for Actor {
     fn get_id(&self) -> Option<crate::engine::tracking_worldlayer::TrackableId> {
         Some(TrackableId(self.actor_id.idx))
     }
+}
+
+pub fn update_paradox(
+    actor: TrackableId,
+    increment: f64,
+    game: &Game,
+) -> Result<(GameUpdate, bool)> {
+    let mut update = GameUpdate::new();
+    let location = *update
+        .world
+        .actor_updates
+        .get_location(&game.world.actors, &actor)?;
+    let maybe_actor = update
+        .world
+        .actor_updates
+        .get(&game.world.actors, &location)?;
+    let mut actor = maybe_actor
+        .as_ref()
+        .ok_or(Error("No actor at expected coordinates"))
+        .cloned()?;
+
+    if increment > 0.0 {
+        actor.paradox_level += increment
+    } else {
+        actor.paradox_level -= 1.0;
+        if actor.paradox_level < 0.0 {
+            actor.paradox_level = 0.0;
+        }
+    }
+    update.world.actor_updates.set(&location, &Some(actor))?;
+    Ok((update, actor.paradox_level < 10.0))
 }
--- src/game_state/game.rs
+++ src/game_state/game.rs
@@ -2,28 +2,28 @@
 
 use ratatui::DefaultTerminal;
 
+use crate::actor::Actor;
 use crate::engine::tracking_worldlayer::TrackableId;
 use crate::engine::update::{Delta, Updatable, UpdatableContainer};
 use crate::error::StatusMenu;
 use crate::interface::menu::{MenuTrait, UILayer};
 use crate::recording::interface::RecordingModule;
-use crate::score::{Score, ScoreDelta};
-use crate::{action, devtools};
-use crate::actor::{Actor};
-use crate::static_data::StaticData;
 use crate::recording::Recording;
+use crate::score::{Score, ScoreDelta};
+use crate::static_data::StaticData;
+use crate::{action, actor, devtools};
 
-use crate::game_state::db::{ ActorId};
 use crate::error::{
     Result,
     Status::{ActionFail, Error},
 };
+use crate::eventqueue::{ActorEvent, EventQueue, EventQueueUpdate};
+use crate::game_state::db::ActorId;
 use crate::{
     datatypes::Coordinate,
     game_state::world::{World, WorldUpdate},
 };
 use std::cell::RefCell;
-use crate::eventqueue::{EventQueue, EventQueueUpdate};
 
 // Move the WorldActors struct out to a dedicated module.
 pub struct WorldActors {
@@ -36,9 +36,7 @@ pub struct PlayerRef {
 
 impl WorldActors {
     pub fn new() -> WorldActors {
-        WorldActors {
-            player: None,
-        }
+        WorldActors { player: None }
     }
 
     pub fn get_player(&self) -> Result<TrackableId> {
@@ -54,12 +52,12 @@ pub struct Game {
     pub world: World,
     pub actors: WorldActors,
     pub recordings: RecordingModule,
-    pub event_queue: EventQueue, 
-    pub data: &'static  StaticData,
-    pub score: Score
+    pub event_queue: EventQueue,
+    pub data: &'static StaticData,
+    pub score: Score,
 }
 
-impl Updatable for Game{}
+impl Updatable for Game {}
 
 // A container that stores game updates.
 // Most operations on the game can be performed with an immutable game and a mutable update.
@@ -78,7 +76,7 @@ impl Delta for GameUpdate {
         GameUpdate {
             world: WorldUpdate::new(),
             eventqueue: EventQueueUpdate::new(),
-            score: ScoreDelta(0)
+            score: ScoreDelta(0),
         }
     }
 
@@ -91,10 +89,21 @@ impl Delta for GameUpdate {
 }
 
 pub trait ApplyOrPopup {
-    fn apply_or_popup(self, game: &RefCell<Game>, parent: &dyn UILayer,  terminal: &mut DefaultTerminal);    }
+    fn apply_or_popup(
+        self,
+        game: &RefCell<Game>,
+        parent: &dyn UILayer,
+        terminal: &mut DefaultTerminal,
+    );
+}
 
 impl ApplyOrPopup for Result<GameUpdate> {
-    fn apply_or_popup(self, game: &RefCell<Game>, parent: &dyn UILayer,  terminal: &mut DefaultTerminal) {
+    fn apply_or_popup(
+        self,
+        game: &RefCell<Game>,
+        parent: &dyn UILayer,
+        terminal: &mut DefaultTerminal,
+    ) {
         match self {
             Ok(update) => {
                 let new_result = update.apply(&mut game.borrow_mut());
@@ -102,9 +111,12 @@ impl ApplyOrPopup for Result<GameUpdate> {
                     StatusMenu::new(err, parent).enter_menu(terminal);
                     panic!("Error applying game update.")
                 }
-            },
+            }
             Err(ActionFail(msg)) => StatusMenu::new(ActionFail(msg), parent).enter_menu(terminal),
-            Err(status) => {StatusMenu::new(status, parent).enter_menu(terminal); panic!("Uncaught error when generating game update.")}
+            Err(status) => {
+                StatusMenu::new(status, parent).enter_menu(terminal);
+                panic!("Uncaught error when generating game update.")
+            }
         };
     }
 }
@@ -117,18 +129,20 @@ impl Game {
             recordings: RecordingModule::new(),
             event_queue: EventQueue::new(),
             data: data,
-            score: Score(0)
+            score: Score(0),
         }
     }
 
     pub fn get_player_actor(&self) -> Result<&Actor> {
         let location = self.get_player_coords()?;
         match self.world.actors.get(&location) {
-            Err(crate::error::Status::OutOfBounds) => Err(Error("Player coordinates out of bounds")),
+            Err(crate::error::Status::OutOfBounds) => {
+                Err(Error("Player coordinates out of bounds"))
+            }
             Err(foo) => Err(foo),
             Ok(None) => Err(Error("No actor at player coordinates")),
             Ok(Some(actor)) => Ok(actor),
-         }
+        }
     }
 
     pub fn get_player_coords(&self) -> Result<&Coordinate> {
@@ -146,44 +160,59 @@ impl Game {
             return Err(Error("Destination Occupied"));
         }
         let mut new_actor = Actor::new_player();
-    
+
         let player_id = self.world.actors.mut_get_next_id();
-        new_actor.actor_id = ActorId{idx: player_id.0};
+        new_actor.actor_id = ActorId { idx: player_id.0 };
 
         self.actors.player = Some(PlayerRef {
             actor_id: player_id,
         });
-        self.world.actors.mut_set(location,&Some(new_actor))
+        self.world.actors.mut_set(location, &Some(new_actor))
+    }
+
+    fn npc_turn(&mut self, mut evt: ActorEvent) -> Result<()> {
+        let recording: &Recording = self.recordings.get(evt.recording);
+        // handle looping here.
+        let action = recording.at(evt.recording_idx);
+
+        let action_result = action::execute_action(evt.actor, action, self);
+
+        let mut live = true;
+
+        match action_result {
+            Ok(update) => {
+                update.apply(self)?;
+                actor::update_paradox(evt.actor, 0.0, self)?.0.apply(self)?;
+            }
+            Err(ActionFail(_)) => {
+                let (update, survivable) = actor::update_paradox(evt.actor, 2.0, self)?;
+                live = survivable;
+                update.apply(self)?;
+            } // call fallback action
+            Err(res) => Err(res)?,
+        };
+
+        let recording: &Recording = self.recordings.get(evt.recording);
+        evt.recording_idx += 1;
+        if evt.recording_idx >= recording.len() {
+            if recording.should_loop {
+                evt.recording_idx = evt.recording_idx % recording.len();
+            } else {
+                live = false;
+            }
+        }
+        if live {
+            self.event_queue.next_turn.push_back(evt);
+        } else {
+            let update = devtools::despawn_actor(evt.actor, &self)?;
+            update.apply(self).unwrap();
+        }
+        Ok(())
     }
 
     pub fn do_npc_turns(&mut self) -> Result<()> {
-        while let Some(mut evt) = self.event_queue.get_next_event() {
-            let recording: &Recording = self.recordings.get(evt.recording);
-            // handle looping here.
-            let action = recording.at(evt.recording_idx);
-
-            let action_result = action::execute_action(evt.actor, action, self);
-
-
-            let ret = match action_result {
-                Ok(update) => update.apply(self),
-                Err(ActionFail(_)) => Ok(()), // call fallback action
-                Err(res) => Err(res),
-            }?;
-
-            let recording: &Recording = self.recordings.get(evt.recording);
-            evt.recording_idx += 1;
-            if evt.recording_idx >= recording.len() {
-                if recording.should_loop {
-                    evt.recording_idx = evt.recording_idx % recording.len();
-                    self.event_queue.next_turn.push_back(evt);
-                } else {
-                    let update = devtools::despawn_actor(evt.actor, &self)?;
-                    update.apply(self).unwrap();
-                }
-            } else {
-                self.event_queue.next_turn.push_back(evt);
-            }
+        while let Some(evt) = self.event_queue.get_next_event() {
+            self.npc_turn(evt)?;
         }
         Ok(())
     }
@@ -194,7 +223,10 @@ impl Game {
 
         match action::execute_action(actor_ref, action, self) {
             // TODO: shove in an apply or popup here.
-            Ok(update) => {self.recordings.append(action);  update.apply(self)},
+            Ok(update) => {
+                self.recordings.append(action);
+                update.apply(self)
+            }
             //Err(ActionFail(_)) => Ok(()), // Call fallback action.
             Err(res) => Err(res),
         }
@@ -212,7 +244,11 @@ impl Game {
 mod tests {
     use action::{Action, SubAction};
 
-    use crate::{devtools, direction::{AbsoluteDirection, Direction::Absolute}, inventory::Item};
+    use crate::{
+        devtools,
+        direction::{AbsoluteDirection, Direction::Absolute},
+        inventory::Item,
+    };
 
     use super::*;
 
@@ -226,11 +262,10 @@ mod tests {
         let recorder_def = data.items.get(&"recorder".to_string()).unwrap();
         let item = Item::new(recorder_def, 1);
         let update = devtools::grant_item(item, *game.get_player_coords().unwrap(), &game).unwrap();
-        
+
         update.apply(&mut game).unwrap();
 
         RecordingModule::init_record(&mut game, 0).unwrap();
-
 
         let actions = [
             Action {
@@ -250,11 +285,12 @@ mod tests {
         let update = RecordingModule::take_item(&mut game).unwrap();
         update.apply(&mut game).unwrap();
 
-
         // This is really ugly. Perhaps recording needs a nicer API.
         let actor = game
             .world
-            .actors.get(&game.get_player_coords().unwrap()).unwrap();
+            .actors
+            .get(&game.get_player_coords().unwrap())
+            .unwrap();
         let recorder = actor.unwrap().inventory.get_items()[0].unwrap();
         let recoding = game.recordings.get(recorder.recording.unwrap());
         assert_eq!(recoding.command_list, actions);
@@ -278,12 +314,15 @@ mod tests {
             },
         ];
 
-        let sample_recording_id = game
-        .recordings.recordings
-        .register_recording(Recording{command_list: actions, inventory: Default::default(), should_loop:true});
+        let sample_recording_id = game.recordings.recordings.register_recording(Recording {
+            command_list: actions,
+            inventory: Default::default(),
+            should_loop: true,
+        });
         let cloner_def = data.items.get(&"basic_cloner".to_string()).unwrap();
         let new_cloner = Item::new_cloner(cloner_def, sample_recording_id);
-        let update = devtools::grant_item(new_cloner, *game.get_player_coords().unwrap(), &game).unwrap();
+        let update =
+            devtools::grant_item(new_cloner, *game.get_player_coords().unwrap(), &game).unwrap();
         update.apply(&mut game).unwrap();
 
         let update = action::execute_action(
@@ -297,12 +336,12 @@ mod tests {
         .unwrap();
         update.apply(&mut game).unwrap();
 
-        let dest = game.world.actors.get(&Coordinate{x:0, y:2}).unwrap();
+        let dest = game.world.actors.get(&Coordinate { x: 0, y: 2 }).unwrap();
         assert!(dest.is_none());
 
         game.do_npc_turns().unwrap();
 
-        let dest = game.world.actors.get(&Coordinate{x:0, y:2}).unwrap();
+        let dest = game.world.actors.get(&Coordinate { x: 0, y: 2 }).unwrap();
         assert!(dest.is_some());
     }
 }
