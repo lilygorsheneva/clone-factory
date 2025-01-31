25aafc03a364bc8a6508f98d0b2a98aabda3e251
Lily <lilygorsheneva@gmail.com>
2025-01-31 14:21:51 -0800

    Use arrow keys.

--- src/interface_egui/movement.rs
+++ src/interface_egui/movement.rs
@@ -3,22 +3,22 @@ use crate::{action::{self, Action}, direction::{self, AbsoluteDirection}, game_s
 pub fn movement(game: &mut Game, ctx: &egui::Context) {
     let window = egui::Window::new("Directions").show(ctx, |ui| {
         let button = ui.button("W");
-        if button.clicked() {
-            &game.player_action(Action{direction:direction::Direction::Absolute(AbsoluteDirection::W), action: action::SubAction::Move}).unwrap();
+        if button.clicked() || ui.input(|i| i.key_pressed(egui::Key::ArrowLeft)){
+            &game.player_action_and_turn(Action{direction:direction::Direction::Absolute(AbsoluteDirection::W), action: action::SubAction::Move}).unwrap();
         }
         let button = ui.button("N");
-        if button.clicked() {
-            &game.player_action(Action{direction:direction::Direction::Absolute(AbsoluteDirection::N), action: action::SubAction::Move}).unwrap();
+        if button.clicked() || ui.input(|i| i.key_pressed(egui::Key::ArrowUp)){
+            &game.player_action_and_turn(Action{direction:direction::Direction::Absolute(AbsoluteDirection::N), action: action::SubAction::Move}).unwrap();
         }
         
         let button = ui.button("E");
-        if button.clicked() {
-            &game.player_action(Action{direction:direction::Direction::Absolute(AbsoluteDirection::E), action: action::SubAction::Move}).unwrap();
+        if button.clicked() || ui.input(|i| i.key_pressed(egui::Key::ArrowRight)){
+            &game.player_action_and_turn(Action{direction:direction::Direction::Absolute(AbsoluteDirection::E), action: action::SubAction::Move}).unwrap();
         }
         
         let button = ui.button("S");
-        if button.clicked() {
-            &game.player_action(Action{direction:direction::Direction::Absolute(AbsoluteDirection::S), action: action::SubAction::Move}).unwrap();
+        if button.clicked() || ui.input(|i| i.key_pressed(egui::Key::ArrowDown)){
+            &game.player_action_and_turn(Action{direction:direction::Direction::Absolute(AbsoluteDirection::S), action: action::SubAction::Move}).unwrap();
         }
     });
 }
