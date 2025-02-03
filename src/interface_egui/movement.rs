2facf587096caf936cb3474669d3b73be8724b37
Lily <lilygorsheneva@gmail.com>
2025-02-03 14:41:01 -0800

    Add an error-handler to UI.

--- src/interface_egui/movement.rs
+++ src/interface_egui/movement.rs
@@ -1,24 +1,28 @@
-use crate::{action::{self, Action}, direction::{self, AbsoluteDirection}, game_state::game::Game};
+use crate::{action::{self, Action}, direction::{self, AbsoluteDirection}, game_state::game::Game, Application};
 
-pub fn movement(game: &mut Game, ctx: &egui::Context) {
+pub fn movement(app: &mut Application, ctx: &egui::Context) {
     let window = egui::Window::new("Directions").show(ctx, |ui| {
         let button = ui.button("W");
         if button.clicked() || ui.input(|i| i.key_pressed(egui::Key::ArrowLeft)){
-            &game.player_action_and_turn(Action{direction:direction::Direction::Absolute(AbsoluteDirection::W), action: action::SubAction::Move}).unwrap();
+            app.queue_act(Box::new(|game: &mut Game|
+            game.player_action_and_turn(Action{direction:direction::Direction::Absolute(AbsoluteDirection::W), action: action::SubAction::Move})));
         }
         let button = ui.button("N");
         if button.clicked() || ui.input(|i| i.key_pressed(egui::Key::ArrowUp)){
-            &game.player_action_and_turn(Action{direction:direction::Direction::Absolute(AbsoluteDirection::N), action: action::SubAction::Move}).unwrap();
+            app.queue_act(Box::new(|game: &mut Game|
+            game.player_action_and_turn(Action{direction:direction::Direction::Absolute(AbsoluteDirection::N), action: action::SubAction::Move})));
         }
         
         let button = ui.button("E");
         if button.clicked() || ui.input(|i| i.key_pressed(egui::Key::ArrowRight)){
-            &game.player_action_and_turn(Action{direction:direction::Direction::Absolute(AbsoluteDirection::E), action: action::SubAction::Move}).unwrap();
+            app.queue_act(Box::new(|game: &mut Game|
+            game.player_action_and_turn(Action{direction:direction::Direction::Absolute(AbsoluteDirection::E), action: action::SubAction::Move})));
         }
         
         let button = ui.button("S");
         if button.clicked() || ui.input(|i| i.key_pressed(egui::Key::ArrowDown)){
-            &game.player_action_and_turn(Action{direction:direction::Direction::Absolute(AbsoluteDirection::S), action: action::SubAction::Move}).unwrap();
+            app.queue_act(Box::new(|game: &mut Game|
+            game.player_action_and_turn(Action{direction:direction::Direction::Absolute(AbsoluteDirection::S), action: action::SubAction::Move})));
         }
     });
 }
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use action::Action;
 use datatypes::Coordinate;
 use direction::{AbsoluteDirection, RelativeDirection};
 use eframe::App;
+use error::{Status,Result};
 use game_state::game::Game;
 use interface::{menu::MenuTrait, widgets::WorldWindowWidget};
 use interface_egui::movement::movement;
@@ -30,39 +31,65 @@ mod paradox;
 mod worldgen;
 mod interface_egui;
 
+pub type GameFn = dyn Fn(&mut Game) -> Result<()>;
+
+
 struct Application {
     data: &'static Data,
     game: Rc<RefCell<Game>>,
+    error: Result<()>,
+    command: Option<Box<GameFn>>
 }
+
+
 
 impl Application {
     fn new(cc: &eframe::CreationContext<'_>) -> Self {
         let data = Data::get_config();
         Application {
             data: data,
-            game: worldgen::start_game(data)
+            game: worldgen::start_game(data),
+            error: Ok(()),
+            command: None
         }
     }
+
+    pub fn queue_act(&mut self, command: Box<GameFn>) {
+        if self.error.is_ok() && self.command.is_none() {
+        self.command = Some(command);
+        }
+    }
+
+    pub fn execute(&mut self) {
+        if let Some(cmd) = self.command.as_ref() {
+            let mut game = self.game.borrow_mut();
+            self.error = cmd(&mut game);
+        }
+        self.command = None;
+    }
+
 }
 
 impl eframe::App for Application {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        let mut game = self.game.borrow_mut();
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            movement(&mut game, ctx);
-          
-            
+            movement(self, ctx);
             let painter = ui.painter();
             let area = painter.clip_rect();
+            let game = self.game.borrow_mut();
             let window = WorldWindowWidget::new(&game);
             let shapes = window.paint(area);
             painter.extend(shapes);
         });
+
+        if self.command.is_some() {
+            self.execute();
+        }
     }
 }
 
 fn main() {
     let native_options = eframe::NativeOptions::default();
-    eframe::run_native("My egui App", native_options, Box::new(|cc| Ok(Box::new(Application::new(cc)))));
+    eframe::run_native("My egui App", native_options, Box::new(|cc| Ok(Box::new(Application::new(cc))))).unwrap();
 }
