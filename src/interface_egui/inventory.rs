d587827f50d14015c2fb9ee15a2891e09c98993f
Lily <lilygorsheneva@gmail.com>
2025-02-07 14:54:08 -0800

    Fix inventory keys always using slot 0.

--- src/interface_egui/inventory.rs
+++ src/interface_egui/inventory.rs
@@ -13,27 +13,26 @@ pub fn inventory(app: &mut Application, ctx: &egui::Context) -> Result<()> {
         let items = inventory.get_items();
         ui.horizontal(|ui| {
         for i in 0..items.len() {
-            ui.vertical(|ui| {
+                ui.vertical(|ui| {
                 ui.label(format!("{}", i + 1));
-
                 if let Some(item) = items[i] {
                     let name = &item.definition.text.name;
                     ui.label(name);
                     let button = ui.button("Use");
                     if button.clicked() {
-                        app.queue_act(Box::new(|game: &mut Game| {
+                        app.queue_act(Box::new(move |game: &mut Game| {
                             game.player_action_and_turn(Action {
                                 direction: Direction::Relative(RelativeDirection::F),
-                                action: action::SubAction::Use(0),
+                                action: action::SubAction::Use(i),
                             })
                         }));
                     }
                     let button = ui.button("Drop");
                     if button.clicked() {
-                        app.queue_act(Box::new(|game: &mut Game| {
+                        app.queue_act(Box::new(move |game: &mut Game| {
                             game.player_action_and_turn(Action {
                                 direction: Direction::Relative(RelativeDirection::F),
-                                action: action::SubAction::Drop(0),
+                                action: action::SubAction::Drop(i),
                             })
                         }));
                     }
