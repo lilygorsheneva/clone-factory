6dfc6287f24256afbe7759d693bcd4ba6f727b4a
Lily <lilygorsheneva@gmail.com>
2025-02-09 12:35:30 -0800

    Bind wait button and WASD

--- src/interface_egui/movement.rs
+++ src/interface_egui/movement.rs
@@ -1,39 +1,86 @@
-use crate::{action::{self, Action}, direction::{self, AbsoluteDirection}, game_state::game::Game, Application};
+use crate::{
+    action::{self, Action},
+    direction::{self, AbsoluteDirection},
+    game_state::game::Game,
+    Application,
+};
 
 pub fn movement(app: &mut Application, ctx: &egui::Context) {
     let window = egui::Window::new("Directions").show(ctx, |ui| {
         let button = ui.button("W");
-        if button.clicked() || ui.input(|i| i.key_pressed(egui::Key::ArrowLeft)){
-            app.queue_act(Box::new(|game: &mut Game|
-            game.player_action_and_turn(Action{direction:direction::Direction::Absolute(AbsoluteDirection::W), action: action::SubAction::Move})));
+        if button.clicked()
+            || ui.input(|i| i.key_pressed(egui::Key::ArrowLeft) | i.key_pressed(egui::Key::A))
+        {
+            app.queue_act(Box::new(|game: &mut Game| {
+                game.player_action_and_turn(Action {
+                    direction: direction::Direction::Absolute(AbsoluteDirection::W),
+                    action: action::SubAction::Move,
+                })
+            }));
         }
         let button = ui.button("N");
-        if button.clicked() || ui.input(|i| i.key_pressed(egui::Key::ArrowUp)){
-            app.queue_act(Box::new(|game: &mut Game|
-            game.player_action_and_turn(Action{direction:direction::Direction::Absolute(AbsoluteDirection::N), action: action::SubAction::Move})));
+        if button.clicked()
+            || ui.input(|i| i.key_pressed(egui::Key::ArrowUp) | i.key_pressed(egui::Key::W))
+        {
+            app.queue_act(Box::new(|game: &mut Game| {
+                game.player_action_and_turn(Action {
+                    direction: direction::Direction::Absolute(AbsoluteDirection::N),
+                    action: action::SubAction::Move,
+                })
+            }));
         }
-        
+
         let button = ui.button("E");
-        if button.clicked() || ui.input(|i| i.key_pressed(egui::Key::ArrowRight)){
-            app.queue_act(Box::new(|game: &mut Game|
-            game.player_action_and_turn(Action{direction:direction::Direction::Absolute(AbsoluteDirection::E), action: action::SubAction::Move})));
+        if button.clicked()
+            || ui.input(|i| i.key_pressed(egui::Key::ArrowRight) | i.key_pressed(egui::Key::D))
+        {
+            app.queue_act(Box::new(|game: &mut Game| {
+                game.player_action_and_turn(Action {
+                    direction: direction::Direction::Absolute(AbsoluteDirection::E),
+                    action: action::SubAction::Move,
+                })
+            }));
         }
-        
+
         let button = ui.button("S");
-        if button.clicked() || ui.input(|i| i.key_pressed(egui::Key::ArrowDown)){
-            app.queue_act(Box::new(|game: &mut Game|
-            game.player_action_and_turn(Action{direction:direction::Direction::Absolute(AbsoluteDirection::S), action: action::SubAction::Move})));
+        if button.clicked()
+            || ui.input(|i| i.key_pressed(egui::Key::ArrowDown) | i.key_pressed(egui::Key::S))
+        {
+            app.queue_act(Box::new(|game: &mut Game| {
+                game.player_action_and_turn(Action {
+                    direction: direction::Direction::Absolute(AbsoluteDirection::S),
+                    action: action::SubAction::Move,
+                })
+            }));
+        }
+
+        let button = ui.button("Wait");
+        if button.clicked() || ui.input(|i| i.key_pressed(egui::Key::Space)) {
+            app.queue_act(Box::new(|game: &mut Game| {
+                game.player_action_and_turn(Action {
+                    direction: direction::Direction::Absolute(AbsoluteDirection::S),
+                    action: action::SubAction::Wait,
+                })
+            }));
         }
 
         let button = ui.button("Take");
-        if button.clicked(){
-            app.queue_act(Box::new(|game: &mut Game|
-            game.player_action_and_turn(Action{direction:direction::Direction::Relative(direction::RelativeDirection::F), action: action::SubAction::Take})));
+        if button.clicked() {
+            app.queue_act(Box::new(|game: &mut Game| {
+                game.player_action_and_turn(Action {
+                    direction: direction::Direction::Relative(direction::RelativeDirection::F),
+                    action: action::SubAction::Take,
+                })
+            }));
         }
         let button = ui.button("Interact (building)");
-        if button.clicked(){
-            app.queue_act(Box::new(|game: &mut Game|
-            game.player_action_and_turn(Action{direction:direction::Direction::Relative(direction::RelativeDirection::F), action: action::SubAction::ActivateBuilding})));
+        if button.clicked() {
+            app.queue_act(Box::new(|game: &mut Game| {
+                game.player_action_and_turn(Action {
+                    direction: direction::Direction::Relative(direction::RelativeDirection::F),
+                    action: action::SubAction::ActivateBuilding,
+                })
+            }));
         }
     });
 }
