23889778fd2f2efd891f03f7fd865a62e4aead52
Lily <lilygorsheneva@gmail.com>
2025-02-09 12:29:55 -0800

    Improve recording Menu.

--- src/interface_egui/inventory.rs
+++ src/interface_egui/inventory.rs
@@ -1,4 +1,7 @@
 use crate::error::Result;
+use crate::inventory::Item;
+use crate::recording::interface::RecordingModule;
+use crate::GameFn;
 use crate::{
     action::{self, Action},
     direction::{Direction, RelativeDirection},
@@ -6,39 +9,48 @@ use crate::{
     Application,
 };
 
+fn use_dispatch(idx: usize, item: Item) -> Box<GameFn> {
+    if item.definition.name == "recorder" {
+        return Box::new(move |game: &mut Game| RecordingModule::init_record(game, idx));
+    } else {
+        return Box::new(move |game: &mut Game| {
+            game.player_action_and_turn(Action {
+                direction: Direction::Relative(RelativeDirection::F),
+                action: action::SubAction::Use(idx),
+            })
+        });
+    }
+}
+
 pub fn inventory(app: &mut Application, ctx: &egui::Context) -> Result<()> {
     let inventory = app.game.borrow().get_player_actor()?.inventory;
 
     let window = egui::TopBottomPanel::bottom("Inventory").show(ctx, |ui| {
         let items = inventory.get_items();
         ui.horizontal(|ui| {
-        for i in 0..items.len() {
+            for i in 0..items.len() {
                 ui.vertical(|ui| {
-                ui.label(format!("{}", i + 1));
-                if let Some(item) = items[i] {
-                    let name = &item.definition.text.name;
-                    ui.label(name);
-                    let button = ui.button("Use");
-                    if button.clicked() {
-                        app.queue_act(Box::new(move |game: &mut Game| {
-                            game.player_action_and_turn(Action {
-                                direction: Direction::Relative(RelativeDirection::F),
-                                action: action::SubAction::Use(i),
-                            })
-                        }));
+                    ui.label(format!("{}", i + 1));
+                    if let Some(item) = items[i] {
+                        let name = &item.definition.text.name;
+                        ui.label(name);
+                        let button = ui.button("Use");
+                        if button.clicked() {
+                            app.queue_act(use_dispatch(i, item));
+                        }
+                        let button = ui.button("Drop");
+                        if button.clicked() {
+                            app.queue_act(Box::new(move |game: &mut Game| {
+                                game.player_action_and_turn(Action {
+                                    direction: Direction::Relative(RelativeDirection::F),
+                                    action: action::SubAction::Drop(i),
+                                })
+                            }));
+                        }
                     }
-                    let button = ui.button("Drop");
-                    if button.clicked() {
-                        app.queue_act(Box::new(move |game: &mut Game| {
-                            game.player_action_and_turn(Action {
-                                direction: Direction::Relative(RelativeDirection::F),
-                                action: action::SubAction::Drop(i),
-                            })
-                        }));
-                    }
-                }
-            });
-        }});
+                });
+            }
+        });
     });
     Ok(())
 }
--- src/interface_egui/recording.rs
+++ src/interface_egui/recording.rs
@@ -1,28 +1,37 @@
-use crate::{game_state::game::Game, recording::interface::RecordingModule, Application};
+use crate::{game_state::game::Game, recording::{self, interface::RecordingModule}, Application};
 
 pub struct RecorderMenu {}
 
 impl RecorderMenu {
     pub fn show(app: &mut Application, ctx: &egui::Context) {
         let window = egui::Window::new("Recording").show(ctx, |ui| {
-            if ui.button("Start recording").clicked() {
-                app.queue_act(Box::new(|game: &mut Game| {
-                    RecordingModule::init_record(game, 0)
-                }));
+            let mut now_recording = false;
+            let mut has_recording = false;
+            {
+                let recoding_module = &app.game.borrow().recordings;  
+                if let Some(rec) = &recoding_module.current_recording {
+                    ui.label(format!("Now recording {} steps", rec.len()));
+                    now_recording = true;
+                }
+                has_recording = recoding_module.temp_item.is_some()
             }
-            if ui.button("Loop").clicked() {
+            if now_recording {
+            if ui.button("End current recording; loop.").clicked() {
                 app.queue_act(Box::new(|game: &mut Game| {
                     RecordingModule::end_record(game, true)
                 }));
             }
-            if ui.button("Die").clicked() {
+            if ui.button("End current recording; No loop.").clicked() {
                 app.queue_act(Box::new(|game: &mut Game| {
                     RecordingModule::end_record(game, false)
                 }));
             }
-            if ui.button("Replicate").clicked() {
+        }
+        if has_recording {
+            if ui.button("Add recording to inventory").clicked() {
                 app.queue_act(Box::new(|game: &mut Game|  RecordingModule::take_item(game)));
-            }
+            }}
         });
+        
     }
 }
